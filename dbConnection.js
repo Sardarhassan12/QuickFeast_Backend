8e3896a3a74e7fe8c8c6b82a0f3f21e93f6809fa
Sardar Ali Hassan <135510461+Sardarhassan12@users.noreply.github.com>
2025-05-22 11:05:35 +0500

    changes

--- dbConnection.js
+++ dbConnection.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try{
-        const conn = mongoose.connect(process.env.MONGODB_URI,{
-            useNewUrlParser : true
-        });
+        const conn = mongoose.connect(process.env.MONGODB_URI);
         console.log("Mongo DB Connected");
     }catch(error){
         console.log(`Error in COnnectign DB ${error.message}`);
